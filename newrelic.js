91b26d3a60b49a8daf8579ced65e6a4c210a321e
Eugene Shulepin <eugene.shulepin@gmail.com>
2016-04-06 11:57:15 +0300

    newrelic integration

--- newrelic.js
+++ newrelic.js
@@ -8,11 +8,11 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['pumpkin-tart-71750'],
+  app_name: ['shuler`s app'],
   /**
    * Your New Relic license key.
    */
-  license_key: 'eee61b7f7f91dd27420975c00ae02d8f97bb6922',
+  license_key: '94a2deaf179d1f8ec3c1c98307f982387cb0ada8',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
